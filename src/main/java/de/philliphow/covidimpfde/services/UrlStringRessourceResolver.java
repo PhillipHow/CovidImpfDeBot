fd518f95c257483a48fdceae7cf41b5392e2ee18
PhillipHow <pillaludemus@gmail.com>
2021-05-22 19:05:58 +0200

    Revert "Integrate first/second dose day difference into vaccination update message"
    This reverts commit 7351b46528de40a7f82d9776bc562c5a4a1fda27.

--- src/main/java/de/philliphow/covidimpfde/services/UrlStringRessourceResolver.java
+++ src/main/java/de/philliphow/covidimpfde/services/UrlStringRessourceResolver.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.UnknownHostException;
 
 import org.pmw.tinylog.Logger;
 
